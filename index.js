6719370dccae3a790d29aaef69f8aa29e7a58f43
Anthony Corcutt <anthonycorcutt@Anthonys-MacBook.local>
2018-06-08 21:29:29 +0100

    remove comments

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// Super simple & fast html tag for es6 literals
-// - all non-array variables escaped html`<i>${'<b>Escaped</b>'}</i>`
-// - use array wrapper to unescape a string or results from map html `<i>${['<b>Unescaped</b>']}</i>`
-
 const escaped = {
     "&": "&amp;",
     "<": "&lt;",
@@ -21,7 +17,6 @@ function transform(value) {
 }
 
 function html(strings, ...values) {
-    // Note... i is 1 if initalValue is not provided to reduce so we can use i-1 and avoid length check
     return strings.reduce((prev, current, i) => (prev + transform(values[i - 1]) + current));
 }
 
